0e3a4f3198197d340fcd88b411684c9cc4465c1b
Rogerio Hirooka <rogerio@Rogerios-Mini.localdomain>
2025-12-06 20:35:36 -0600

    Expose full public headers and rebuild XCFramework

--- SampleSPMProject/Package.swift
+++ SampleSPMProject/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/rogerioth/BRFullTextSearch.git", exact: "1.0.5")
+        .package(url: "https://github.com/rogerioth/BRFullTextSearch.git", exact: "1.0.6")
     ],
     targets: [
         .executableTarget(
